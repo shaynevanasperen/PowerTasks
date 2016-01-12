f7807fdb6a50ea57cb6dbc4cef54c81c96a8c3d6
Kenneth Truyers <truyers.kenneth@gmail.com>
2016-01-12 23:35:56 +0100

    Add a plugin to deploy a release on an OctopusDeployServer

---
+++ PowerTasks/.Tasks/OctoDeployRelease.ps1
@@ -0,0 +1,24 @@
+$script:artifactsPath = (property artifactsPath $basePath\artifacts)
+$script:octopusDeployServer = (property octopusDeployServer "")
+$script:octopusDeployApiKey = (property octopusDeployApiKey "")
+$script:octopusDeployProjectName = (property octopusDeployProjectName $projectName)
+$script:environment = (property environment "")
+
+task OctoDeployRelease {
+	if([String]::IsNullOrEmpty($octopusDeployServer)){
+		throw "Please specify the octopusDeployServer"
+	}
+	if([String]::IsNullOrEmpty($octopusDeployApiKey)){
+		throw "Please specify the octopusDeployApiKey"
+	}
+
+	$assemblyInfoFile = Get-Content ".\Properties\AssemblyInfo.cs"
+	$version = $assemblyInfoFile | 
+				where { $_ -match "AssemblyVersion\(`"(?<version>.*)`"\)" } |
+				foreach { $matches["version"] }
+
+	$octopusToolsPath = Get-RequiredPackagePath OctopusTools $basePath\$projectName
+	$cmd = "$octopusToolsPath\Octo.exe deploy-release --server=""$octopusDeployServer"" --apiKey=""$octopusDeployApiKey"" --project=""$octopusDeployProjectName"" --version=""$version"" --deployto=""$environment"""
+
+	exec { & Invoke-Expression $cmd }
+}
