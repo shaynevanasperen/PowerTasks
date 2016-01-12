621bec569a031984d04ad13804114c6a95dddf41
Kenneth Truyers <truyers.kenneth@gmail.com>
2016-01-12 23:20:44 +0100

    Add global function to get projects that have a specific package

--- PowerTasks/Properties/AssemblyInfo.cs
+++ PowerTasks/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.1.1")]
-[assembly: AssemblyFileVersion("2.1.1")]
+[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyFileVersion("3.0.0")]
