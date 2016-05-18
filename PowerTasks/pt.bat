cf4e3cfdbc70519850470f6383655d7631cd04a3
Shayne van Asperen <shaynevanasperen@gmail.com>
2016-05-18 19:47:49 +0100

    Use new download URL for NuGet.exe and update InvokeBuild to latest version

--- PowerTasks/pt.bat
+++ PowerTasks/pt.bat
@@ -3,7 +3,7 @@ powershell -NoProfile -ExecutionPolicy Unrestricted -Command ^
 $ErrorActionPreference = 'Stop'; ^
 if (!(Get-Command NuGet -ErrorAction SilentlyContinue) -and !(Test-Path '%LocalAppData%\NuGet\NuGet.exe')) { ^
 	Write-Host 'Downloading NuGet.exe'; ^
-	(New-Object system.net.WebClient).DownloadFile('https://www.nuget.org/nuget.exe', '%LocalAppData%\NuGet\NuGet.exe'); ^
+	(New-Object system.net.WebClient).DownloadFile('https://dist.nuget.org/win-x86-commandline/latest/nuget.exe', '%LocalAppData%\NuGet\NuGet.exe'); ^
 } ^
 if (Test-Path '%LocalAppData%\NuGet\NuGet.exe') { ^
 	Set-Alias NuGet (Resolve-Path %LocalAppData%\NuGet\NuGet.exe); ^
