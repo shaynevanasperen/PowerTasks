79bc3425a463c795b4cae9b9d3846e5f16e75f5c
Arthur Mehta <arthur.mehta@seatwave.com>
2016-06-22 14:42:21 +0100

    Correct the project variable

--- PowerTasks/.Tasks/XUnit.ps1
+++ PowerTasks/.Tasks/XUnit.ps1
@@ -3,10 +3,10 @@ $script:config = (property config "Release")
 task XUnit {
 	New-Item $artifactsPath -Type directory -Force | Out-Null
 
-	foreach ($projectTest in  @(Get-ProjectsWithPackage("Xunit"))) {
+	foreach ($project in  @(Get-ProjectsWithPackage("Xunit"))) {
 		$xunitRunnerPath = Get-RequiredPackagePath XUnit.Runner.Console "$basePath\$($project.Path)"
 		$runnerExecutable = "$xunitRunnerPath\tools\xunit.console.exe"
-		exec { & $runnerExecutable $basePath\$($projectTest.Path)\bin\$config\$($project.Name).dll -xml "$artifactsPath\xunit.xml" -html "$artifactsPath\xunit.html" -nologo }
+		exec { & $runnerExecutable $basePath\$($project.Path)\bin\$config\$($project.Name).dll -xml "$artifactsPath\xunit.xml" -html "$artifactsPath\xunit.html" -nologo }
 		continue
 	}
 }
