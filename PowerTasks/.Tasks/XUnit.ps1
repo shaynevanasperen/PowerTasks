2c87ddbd55d2e141db458d9d3e7ad130954940cd
Kenneth Truyers <truyers.kenneth@gmail.com>
2016-07-08 20:58:08 +0200

    Bugfix for XUnit test runner

--- PowerTasks/.Tasks/XUnit.ps1
+++ PowerTasks/.Tasks/XUnit.ps1
@@ -4,7 +4,7 @@ task XUnit {
 	New-Item $artifactsPath -Type directory -Force | Out-Null
 
 	foreach ($projectTest in  @(Get-ProjectsWithPackage("Xunit"))) {
-		$xunitRunnerPath = Get-RequiredPackagePath XUnit.Runner.Console "$basePath\$($project.Path)"
+		$xunitRunnerPath = Get-RequiredPackagePath XUnit.Runner.Console "$basePath\$($projectTest.Path)"
 		$runnerExecutable = "$xunitRunnerPath\tools\xunit.console.exe"
 		exec { & $runnerExecutable $basePath\$($projectTest.Path)\bin\$config\$($project.Name).dll -xml "$artifactsPath\xunit.xml" -html "$artifactsPath\xunit.html" -nologo }
 		continue
