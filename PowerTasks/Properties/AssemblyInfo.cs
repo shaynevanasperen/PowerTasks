920208020e52daf70a6cbed91c07745bbcef2611
Kenneth Truyers <truyers.kenneth@gmail.com>
2016-07-19 21:31:37 +0200

    Bump version number

--- PowerTasks/Properties/AssemblyInfo.cs
+++ PowerTasks/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("3.1.0")]
-[assembly: AssemblyFileVersion("3.1.0")]
+[assembly: AssemblyVersion("3.2.0")]
+[assembly: AssemblyFileVersion("3.2.0")]
