32f2298060047315b7ebca8eb005d62a28c46f65
Kenneth Truyers <truyers.kenneth@gmail.com>
2016-07-27 13:56:33 +0200

    Added possibility to include branch, commit, shortCommit and full month in the version number

--- PowerTasks/.Tasks/Version.ps1
+++ PowerTasks/.Tasks/Version.ps1
@@ -10,15 +10,38 @@ function script:Set-Version($projectPath, $version) {
 		
 		$year = $(Get-date -f yyyy)
 		$month = $(Get-date -f %M)
+		$fullMonth = $(Get-date -f MM)
 		$quarter = $([Math]::ceiling((Get-date -f MM)/3))
 		$date = $(Get-date -f dd)
 
-		$version = $version.Replace("{year}", $year).Replace("{month}", $month).Replace("{quarter}", $quarter).Replace("{date}", $date)
+		$version = $version.Replace("{year}", $year).Replace("{month}", $month).Replace("{fullMonth}", $fullMonth).Replace("{quarter}", $quarter).Replace("{date}", $date)
+		$assemblyVersion = $version.Clone()
+		$assemblyVersion = $assemblyVersion.Replace("{commit}", "").Replace("{shortCommit}", "").Replace("{branch}", "").Replace("-", "")
+		Write-Host $assemblyVersion
+		if($version.Contains("{branch}") -or $version.Contains("commit")){
+			pushd ..
+			
+			try {
+				$branch = git branch | Where {$_ -match "^\*(.*)"} | Select-Object -First 1
+				$branch = $branch.Replace("* ", "").Replace("/", "-")
+
+				$commit = git rev-parse HEAD
+				$shortCommit = $commit.Substring(0,7)
+
+				$version = $version.Replace("{branch}", $branch).Replace("{commit}", $commit).Replace("{shortCommit}", $shortCommit)
+			}
+			catch{
+				Write-Host "Could not determine branch and commit hash through git"
+			}
+			
+			popd
+		}
+		
 
 		if ((Test-Path $assemblyInfoFile)) {
-			Write-Host "Updating $assemblyInfoFile"
-			$newAssemblyVersion = 'AssemblyVersion("' + $version + '")'
-			$newAssemblyFileVersion = 'AssemblyFileVersion("' + $version + '")'
+			Write-Host "Updating $assemblyInfoFile with $assemblyVersion"
+			$newAssemblyVersion = 'AssemblyVersion("' + $assemblyVersion + '")'
+			$newAssemblyFileVersion = 'AssemblyFileVersion("' + $assemblyVersion + '")'
 			$newFileContent = Get-Content $assemblyInfoFile |
 				%{ $_ -replace 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', $newAssemblyVersion } |
 				%{ $_ -replace 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', $newAssemblyFileVersion }
@@ -29,5 +52,6 @@ function script:Set-Version($projectPath, $version) {
 		Write-Host "Getting version from $assemblyInfoFile"
 		$version = Get-AssemblyFileVersion $assemblyInfoFile
 	}
+	Write-Host "Version obtained: $version"
 	return $version
 }
