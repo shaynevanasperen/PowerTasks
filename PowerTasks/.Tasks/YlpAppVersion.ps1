7b1e147cbdaad32260de0598bf23fd2a7768cb22
Kenneth Truyers <truyers.kenneth@gmail.com>
2016-07-29 16:04:51 +0200

    Add a new version task for Ylp Apps

---
+++ PowerTasks/.Tasks/YlpAppVersion.ps1
@@ -0,0 +1,36 @@
+$script:version = (property version "")
+
+task YlpAppVersion {
+	$script:version = Set-Version . $version
+}
+
+function script:Set-Version($projectPath, $version) {
+	$assemblyInfoFile = "$projectPath\Properties\AssemblyInfo.cs"
+	
+	if([String]::IsNullOrEmpty($env:APPVEYOR_BUILD_NUMBER )){
+		$buildNumber = (Get-Date).ticks.ToString()
+		$buildNumber = $buildNumber.Substring($buildNumber.Length - 10)
+	}else{
+		$buildNumber = $env:APPVEYOR_BUILD_NUMBER
+	}
+
+	pushd ..
+	$shortCommit = $(git rev-parse HEAD).Substring(0,7)
+	popd
+		
+	$version = "$(Get-date -f yyyy).$(Get-date -f MM).$(Get-date -f dd)-v$buildNumber-$shortCommit"
+	$assemblyVersion = "$(Get-date -f yyyy).$(Get-date -f MM).$(Get-date -f dd).$buildNumber"
+
+	if ((Test-Path $assemblyInfoFile)) {
+		Write-Host "Updating $assemblyInfoFile with $assemblyVersion"
+		$newAssemblyVersion = 'AssemblyVersion("' + $assemblyVersion + '")'
+		$newAssemblyFileVersion = 'AssemblyFileVersion("' + $assemblyVersion + '")'
+		$newFileContent = Get-Content $assemblyInfoFile |
+			%{ $_ -replace 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', $newAssemblyVersion } |
+			%{ $_ -replace 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', $newAssemblyFileVersion }
+		Set-Content $assemblyInfoFile $newFileContent
+	}
+	
+	Write-Host "Version obtained: $version"
+	return $version
+}
